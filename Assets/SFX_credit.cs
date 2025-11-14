349b5737fed74634e0214242934ac1dc1ef5e52a
KevX36 <kevincard2007@gmail.com>
2025-11-14 14:44:03 -0400

    set up timeline (need to set to trigger)

---
+++ Assets/SFX_credit.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class SFX_credit : MonoBehaviour
+{
+    //Sound Effect by <a href="https://pixabay.com/users/dragon-studio-38165424/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=419014">DRAGON-STUDIO</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=419014">Pixabay</a>
+}
